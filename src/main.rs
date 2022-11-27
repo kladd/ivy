2ff51be919bc719c0a5dff057f0ddbf343908cbc
Kyle Ladd <kyle@ladd.io>
2022-11-27 15:19:11 -0500

    Set multiboot mem info flag

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,7 @@ pub extern "C" fn start() -> ! {
 #[link_section = ".multiboot"]
 static _MULTIBOOT_MAGIC: i32 = 0x1BADB002;
 #[link_section = ".multiboot"]
-static _MULTIBOOT_ARCH: i32 = 0;
+// ALIGN | MEM_INFO
+static _MULTIBOOT_FLAGS: i32 = 3;
 #[link_section = ".multiboot"]
-static _MULTIBOOT_CHK: i32 = -_MULTIBOOT_MAGIC;
+static _MULTIBOOT_CHK: i32 = -(_MULTIBOOT_MAGIC + _MULTIBOOT_FLAGS);
