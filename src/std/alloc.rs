19cb92836886f8e9bf8a03ef2cdec545f471014f
Kyle Ladd <kyle@ladd.io>
2023-04-09 13:34:23 -0400

    Align all allocs

--- src/std/alloc.rs
+++ src/std/alloc.rs
@@ -4,13 +4,12 @@ static mut MAX_ADDR: u32 = 0x400000;
 const PAGE_SIZE: u32 = 0x1000;
 
 pub fn kmalloc_aligned(size: usize) -> u32 {
-	unsafe {
-		PLACEMENT_ADDR = (PLACEMENT_ADDR & !(PAGE_SIZE - 1)) + PAGE_SIZE;
-		kmalloc(size)
-	}
+	align(PAGE_SIZE);
+	kmalloc(size)
 }
 
 pub fn kmalloc(size: usize) -> u32 {
+	align(usize::BITS);
 	unsafe {
 		let placement = PLACEMENT_ADDR;
 		let next_placement = placement + size as u32;
@@ -22,4 +21,8 @@ pub fn kmalloc(size: usize) -> u32 {
 			0
 		}
 	}
+}
+
+fn align(alignment: u32) {
+	unsafe { PLACEMENT_ADDR = (PLACEMENT_ADDR & !(alignment - 1)) + alignment };
 }
