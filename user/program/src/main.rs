c1c4b725253ec29ea6ab7dc29302460cf4ab9cba
Kyle Ladd <kyle@ladd.io>
2023-12-06 04:09:14 -0500

    Add a crt0-like _start to prelude

--- user/program/src/main.rs
+++ user/program/src/main.rs
@@ -1,18 +1,10 @@
 #![no_std]
 #![no_main]
 
-use core::arch::global_asm;
-
-use sys::syscall::{exit, video_test};
-
-global_asm! {
-	".global _start",
-	"_start:",
-	"call main"
-}
+use sys::syscall::video_test;
 
 #[no_mangle]
 fn main(_argc: isize, _argv: *const *const u8) -> isize {
 	video_test();
-	exit();
+	0
 }
--- user/sys/src/prelude.rs
+++ user/sys/src/prelude.rs
@@ -1,6 +1,15 @@
-use core::panic::PanicInfo;
+use core::{arch::global_asm, panic::PanicInfo};
 
 use crate::syscall::exit;
+
+global_asm! {
+	".global _start",
+	"_start:",
+	"call main",
+	"mov rdi, rax",
+	"mov eax, 60",
+	"syscall"
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
